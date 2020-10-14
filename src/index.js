df7680569f71bd050b86ba52e64a9b16aebc398e
Youness BENNAJ <youness.bennaj@gmail.com>
2020-10-14 14:02:02 +0200

    init our first component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello World !');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <h1>Hello World!</h1>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
